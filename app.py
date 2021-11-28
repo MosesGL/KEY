f684194a581aca4d19f9b3e0f378347826802689
Moses <MosesGL.code@gmail.com>
2021-11-27 19:06:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, config, render_template, request, url_for
+from flask import Flask, render_template, request, url_for
 import mido
 from threading import Thread
 # Song list is stored in csv format
